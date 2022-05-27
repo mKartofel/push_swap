6870f1113bf4fb8f09a37079b7cafa9a97c6631d
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-05-27 11:05:57 +0200

    Quicksort sorting

---
+++ quicksort.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quicksort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 10:58:29 by vfiszbin          #+#    #+#             */
+/*   Updated: 2022/05/27 11:03:09 by vfiszbin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	swap(int *t, int i, int j){ 
+	int tmp = t[i];
+	t[i] = t[j];
+	t[j] = tmp;
+}
+
+int	quicksort_separation(int *t, int begin, int end) 
+{ 
+	int i,sep = begin + 1;
+	for(i=begin+1;i<=end;i++)
+		if (t[i] < t[begin]) 
+		{
+			if (i!=sep) swap(t,i,sep); sep ++;
+		}
+	if (sep!=begin+1) 
+	{ 
+		swap(t,begin,sep-1);
+	}		
+	return sep - 1; 
+}
+
+/*Sort an int array using the Quicksort Algorithm*/
+int quicksort(int *t, int begin, int end) 
+{ 
+	if (begin<end) 
+	{
+		int milieu = quicksort_separation(t, begin, end); 
+		quicksort(t,begin,milieu); 
+		quicksort(t,milieu+1,end);
+	}
+	return (0);
+}
+
+
+// #include <stdio.h>
+
+// void print_tab(int *tab, int len)
+// {
+// 	for (int i = 0; i < len; i++)
+// 		printf("%d ", tab[i]);
+// 	printf("\n");
+// }
+
+
+// int main(){
+// 	int tab[5] = {4,2,5,3,7};
+// 	print_tab(tab, 5);
+// 	quicksort(tab, 0, 4);
+// 	print_tab(tab, 5);
+
+// }
